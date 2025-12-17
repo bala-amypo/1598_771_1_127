8561512210f85c8a462035b989e09a61b475409e
bala-amypo <bala@amypo.in>
2025-12-17 09:31:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,3 +6,35 @@ public class User {
     private String email;
     private String password;
 }
+
+
+
+
+package com.example.demo.entity;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+public class Complaint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String category;
+    private Integer priorityScore;
+
+    private LocalDateTime submittedOn;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @PrePersist
+    public void onCreate() {
+        this.submittedOn = LocalDateTime.now();
+    }
+}
