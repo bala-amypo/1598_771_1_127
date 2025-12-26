54f8bb0ca62711f69884bb55077be71c000524aa
bala-amypo <bala@amypo.in>
2025-12-26 16:50:25 +0000

    User commit

--- src/main/java/com/example/demo/service/PriorityRuleService.java
+++ src/main/java/com/example/demo/service/PriorityRuleService.java
@@ -11,16 +11,3 @@ public interface PriorityRuleService {
 
     List<PriorityRule> getActiveRules();
 }
-package com.example.demo.service;
-
-import com.example.demo.entity.Complaint;
-import com.example.demo.entity.PriorityRule;
-
-import java.util.List;
-
-public interface PriorityRuleService {
-
-    int computePriorityScore(Complaint complaint);
-
-    List<PriorityRule> getActiveRules();
-}
