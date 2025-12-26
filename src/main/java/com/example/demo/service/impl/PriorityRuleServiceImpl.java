8b30f1198d7010b0ce8456d307785f4e16dba226
bala-amypo <bala@amypo.in>
2025-12-26 07:04:16 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/PriorityRuleServiceImpl.java
@@ -22,26 +22,12 @@ public class PriorityRuleServiceImpl implements PriorityRuleService {
 
         int score = 0;
 
-        if (complaint.getSeverity() != null) {
-            score += complaint.getSeverity().ordinal() + 1;
-        }
-
-        if (complaint.getUrgency() != null) {
-            score += complaint.getUrgency().ordinal() + 1;
-        }
-
         List<PriorityRule> rules = priorityRuleRepository.findByActiveTrue();
 
         for (PriorityRule rule : rules) {
             score += rule.getWeight();
-            complaint.getPriorityRules().add(rule);
         }
 
         return score;
-    }
-
-    @Override
-    public List<PriorityRule> getActiveRules() {
-        return priorityRuleRepository.findByActiveTrue();
     }
 }
